5fa35c8975a69699b3061e155cb3c80a41952fab
Rob Tillaart <rob.tillaart@gmail.com>
2024-02-05 16:10:38 +0100

    fix

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -40,8 +40,49 @@ unittest(test_constants)
 {
   assertEqual(0,   DS3232_OK);
   assertEqual(-10, DS3232_ERROR_ADDR);
-  assertEqual(-10, DS3232_ERROR_I2C);
-  assertEqual(-10, DS3232_ERROR_CONNECT);
+  assertEqual(-11, DS3232_ERROR_I2C);
+  assertEqual(-12, DS3232_ERROR_CONNECT);
+}
+
+
+unittest(test_registers)
+{
+  assertEqual(0x00, DS3232_SECONDS);
+  assertEqual(0x07, DS3232_ALARM1);
+  assertEqual(0x0B, DS3232_ALARM2);
+  assertEqual(0x0E, DS3232_CONTROL);
+  assertEqual(0x10, DS3232_AGING_OFFSET);
+  assertEqual(0x11, DS3232_TEMPERATURE);
+  assertEqual(0x14, DS3232_SRAM_BASE);
+}
+
+
+unittest(test_getters_setters)
+{
+  DS3232 rtc;
+
+  assertEqual(0, rtc.seconds());
+  assertEqual(0, rtc.minutes());
+  assertEqual(0, rtc.hours());
+  assertEqual(0, rtc.weekday());
+  assertEqual(0, rtc.day());
+  assertEqual(0, rtc.month());
+  assertEqual(0, rtc.year());
+
+  rtc.setSeconds(1);
+  rtc.setMinutes(2);
+  rtc.setHours(3);
+  rtc.setDay(4);
+  rtc.setMonth(5);
+  rtc.setYear(6);
+
+  assertEqual(1, rtc.seconds());
+  assertEqual(2, rtc.minutes());
+  assertEqual(3, rtc.hours());
+  assertEqual(0, rtc.weekday());
+  assertEqual(4, rtc.day());
+  assertEqual(5, rtc.month());
+  assertEqual(6, rtc.year());
 }
 
 
